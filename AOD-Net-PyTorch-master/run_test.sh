4dc0f0b3446e5361727570e90a696771666833b6
wangpengyu0829 <40031042+wangpengyu0829@users.noreply.github.com>
2021-04-16 23:06:35 +0800

    Delete AOD-Net-PyTorch-master directory

--- AOD-Net-PyTorch-master/run_test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-python demo.py  --net_name aod \
-                --use_gpu true \
-                --gpu 3 \
-                --model_dir /home/wc/workspace/AOD-Net.pytorch/models \
-                --ckpt AOD_9.pkl
